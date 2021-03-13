b0064e2ab0573462ce177a5d30f93660335ade2d
Andrii Tsvil <andriytsvil@gmail.com>
2021-03-13 15:18:57 +0200

    LFS update build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-cd blog
-sudo chown -R $USER:www-data storage
-sudo chown -R $USER:www-data bootstrap/cache
+cd blog || exit
+composer install
+sudo chown -R "$USER":www-data storage
+sudo chown -R "$USER":www-data bootstrap/cache
 sudo chmod -R 775 storage
 sudo chmod -R 775 bootstrap/cache
 php artisan cache:clear
