0a18770bb2eb3b191b852d78d81346e812631930
Andrii Tsvil <andriytsvil@gmail.com>
2021-10-03 16:45:10 +0300

    LFS Section-14 episode-56

---
+++ blog/resources/views/_tweet.blade.php
@@ -0,0 +1,19 @@
+<div class="flex p-4 border-b border-b gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+                src="images/default-avatar.jpeg"
+                alt=""
+                class="rounded-full mr-2"
+                style="width:40px;height:40px"
+        >
+
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+        <p class="text-sm">Presumably, if you're watching this series, you've already made the decision to embrace all
+            that Laravel has to offer. However, if you're still on the fence, give me just a moment to sell you on why I
+            believe Laravel is the best framework choice in the PHP world.
+        </p>
+    </div>
+</div>
