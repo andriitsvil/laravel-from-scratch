a4333462186537d8951f4a7c5985bc998eaa0a9d
Andrii Tsvil <andriytsvil@gmail.com>
2021-10-10 17:31:26 +0300

    LFS Section-14 episode-61

--- blog/resources/views/components/master.blade.php
+++ blog/resources/views/components/master.blade.php
@@ -28,21 +28,7 @@
             </h1>
         </header>
     </section>
-    <section class="px-8 py-4">
-        <main class="container mx-auto">
-            <div class="lg:flex lg:justify-between">
-                <div class="lg:w-32">
-                    @include('_sidebar-links')
-                </div>
-                <div class="lg:flex-1 lg:mx-10">
-                   @yield('content')
-                </div>
-                <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
-                    @include('_friends-list')
-                </div>
-            </div>
-        </main>
-    </section>
+    {{ $slot }}
 </div>
 </body>
 </html>
